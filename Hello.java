dbad26f05fff930af7d178412e9f869ba264695e
ThyagarajaBommalleni <37110934+ThyagarajaBommalleni@users.noreply.github.com>
2018-03-20 15:07:03 +0530

    addedd semicoln

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@ public class Hello {
  
            System.out.println("Hello World ..."+sum);
             int j=5,sum;
-            sum=j+i
+            sum=j+i;
       
                 }
           }
