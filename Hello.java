945df81e63be66047585fc47474fb7cf5c1db326
ThyagarajaBommalleni <37110934+ThyagarajaBommalleni@users.noreply.github.com>
2018-03-28 17:00:08 +0530

    changed the condition to 15

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     
    public static void main(String[] args){
      
-      for (int i=1;i<=5;i++){
+      for (int i=1;i<=15;i++){
  
             int j=5,sum;
             sum=j+i;
